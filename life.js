2711e38967e66cc4df3ef6ca037c71914125d38f
hasanibram <hasanibrahim091@gmail.com>
2019-11-01 10:17:41 +0700

    belajar

--- life.js
+++ life.js
@@ -12,7 +12,9 @@ $(document).ready(function(){
 
 // JQ HASAN
 $(document).ready(function(){
-
+	$(".container-fluid .row .col-sm-12 .jumbotron .container #button").click(function(){
+		$(".container-fluid .row .col-sm-12 .jumbotron .container #button").animate({opacity:1.0},"slow");
+	});
 });
 
 
